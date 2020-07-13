a922c34e5e41baf2a4fec2d6fe8032d900fea0a2
Bryan Valerio <bryanvale91@gmail.com>
2020-07-13 12:05:05 -0600

    Initial project, create structure initial to landing page.

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Menu from './components/menu/menu'
+import Home from './components/home/home'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Menu />
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/menu/menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Menu = () => {
+  return (
+    <div>
+      <ul>Home</ul>
+      <ul>About Us</ul>
+      <ul>Gallery</ul>
+      <ul>Contact</ul>
+    </div>
+  );
+};
+
+export default Menu;
